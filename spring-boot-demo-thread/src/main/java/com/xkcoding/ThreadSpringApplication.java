d021a6076dc0bca3a1942c7b8e4af279d1f3fe10
qianjin <1556316829@qq.com>
2020-06-23 14:38:47 +0800

    多线程学习

---
+++ spring-boot-demo-thread/src/main/java/com/xkcoding/ThreadSpringApplication.java
@@ -0,0 +1,20 @@
+package com.xkcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName ThreadSpringApplication
+ * @Description TODO
+ * @Author 钱进
+ * @Date 2020/6/23 14:35
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class ThreadSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadSpringApplication.class, args);
+    }
+
+}
---
+++ spring-boot-demo-thread/src/test/java/com/xkcoding/test/MainTest.java
@@ -0,0 +1,12 @@
+package com.xkcoding.test;
+
+/**
+ * main方法测试
+ * @ClassName MainTest
+ * @Description TODO
+ * @Author 钱进
+ * @Date 2020/6/23 14:37
+ * @Version 1.0
+ **/
+public class MainTest {
+}
