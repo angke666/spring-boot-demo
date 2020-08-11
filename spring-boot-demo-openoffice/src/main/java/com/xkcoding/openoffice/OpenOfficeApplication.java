d8d4d9b0a2942874499c20185c9bded324a9147b
qianjin <1556316829@qq.com>
2020-08-11 17:26:59 +0800

    整合open office，转换文件成pdf

---
+++ spring-boot-demo-openoffice/src/main/java/com/xkcoding/openoffice/OpenOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.xkcoding.openoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenOfficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenOfficeApplication.class, args);
+    }
+
+}
---
+++ spring-boot-demo-openoffice/src/main/java/com/xkcoding/openoffice/controller/FileController.java
@@ -0,0 +1,69 @@
+package com.xkcoding.openoffice.controller;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+import org.jodconverter.DocumentConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/file")
+public class FileController {
+
+    @Autowired
+    private DocumentConverter converter;
+
+    @PostMapping("/toPdf")
+    public String toPdf(@RequestParam("file") MultipartFile file, HttpServletResponse response) {
+        try {
+            File newFile = new File("D:/Work/Resource/file/pdf");
+            if (!newFile.exists()) {
+                newFile.mkdirs();
+            }
+
+            // 编码文件名
+            String extension = FilenameUtils.getExtension(file.getOriginalFilename());
+            String name = UUID.randomUUID().toString();
+            String fileName = name + "." + extension;
+
+            // 保存原文件
+            File desc = new File("D:/Work/Resource/file" + File.separator + fileName);
+            if (!desc.getParentFile().exists())
+            {
+                desc.getParentFile().mkdirs();
+            }
+            if (!desc.exists())
+            {
+                desc.createNewFile();
+            }
+            file.transferTo(desc);
+
+            // 开始转换
+            String pdfFileName = desc.getParent() + File.separator + name + ".pdf";
+            converter.convert(desc).to(new File(pdfFileName)).execute();
+
+            ServletOutputStream outputStream = response.getOutputStream();
+            FileInputStream inputStream = new FileInputStream(new File(pdfFileName));
+            int i = IOUtils.copy(inputStream, outputStream);
+            System.out.println(i);
+            inputStream.close();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "success";
+    }
+
+}
